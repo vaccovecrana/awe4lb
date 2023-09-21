bf7087756182c389112b05e04ee42aea52c03268
Jesus Zazueta <jjzazuet@gmail.com>
2023-09-21 00:27:55 -0400

    - WIP

--- a4lb-core/src/test/java/io/vacco/a4lb/A4SelTest.java
+++ a4lb-core/src/test/java/io/vacco/a4lb/A4SelTest.java
@@ -1,6 +1,7 @@
 package io.vacco.a4lb;
 
 import com.google.gson.Gson;
+import io.vacco.a4lb.cfg.A4Backend;
 import io.vacco.a4lb.sel.A4Sel;
 import io.vacco.a4lb.util.A4Configs;
 import j8spec.annotation.DefinedOrder;
@@ -15,8 +16,8 @@ public class A4SelTest {
   static {
     it("Selects backend matches", () -> {
       var cfg = A4Configs.loadFrom(A4ValidTest.class.getResource("/config.json"), new Gson());
-
       var srv = cfg.servers.get("vacco-tls");
+      srv.allBackends().forEach(bk -> bk.state = A4Backend.State.Up);
       var mat = new A4Sel(srv.match);
       var host = "172.16.0.111";
       var bk = mat.select(host, host.hashCode(), "momo.xio.vacco.li");
