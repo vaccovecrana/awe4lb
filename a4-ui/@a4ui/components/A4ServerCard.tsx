b385c47f8a8d448344bdb411a9c564001f04bb20
Jesus Zazueta <jjzazuet@gmail.com>
2023-10-15 22:26:37 -0400

    - WIP

--- a4-ui/@a4ui/components/A4ServerCard.tsx
+++ a4-ui/@a4ui/components/A4ServerCard.tsx
@@ -13,8 +13,23 @@ const A4ServerCard = (props: A4ScProps) => (
   <div class="col xs-12 sm-12 md-6">
     <div class="card minimal p8 m2 mt8">
       <div class="card-title-2">
-      <i class="icono-caretRight" /> {props.srv.id}
+      <i class="icono-rss icoMed" /> {props.srv.id}
       </div>
+      {props.srv.tls ? (
+        <div class="mt8 txSmall card minimal">
+          <div>
+            <i class="icono-document icoSmall" /> <span>{props.srv.tls.certPath}</span>
+          </div>
+          <div>
+            <i class="icono-asterisk icoSmall" /> <span>{props.srv.tls.keyPath}</span>
+          </div>
+          {props.srv.tls.ciphers && props.srv.tls.ciphers.length > 0 ? (
+            <div>
+              <i class="icono-hamburger icoSmall" /> {props.srv.tls.ciphers.join(", ")}
+            </div>
+          ) : []}
+        </div>
+      ) : []}
       {props.srv.match.map((match, k) => (
         <div class="mt8">
           {(match.and || match.or) ? (
