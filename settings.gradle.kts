b91461e70ed8b8c54b82dce5a7f79df27ce7a7e9
Jesus Zazueta <jjzazuet@gmail.com>
2024-10-20 12:55:06 -0400

    - Tweaks

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -7,4 +7,4 @@ pluginManagement {
 
 include("a4-core", "a4-ui", "a4-lb", "a4-test")
 
-project(":a4-lb").name = "a4lb-${System.getProperty("os.name").lowercase()}-${System.getProperty("os.arch")}"
+project(":a4-lb").name = "a4lb-${System.getProperty("os.name").lowercase().replace(" ", "")}-${System.getProperty("os.arch")}"
