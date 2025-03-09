f20ba33e2b7308370d7bcd60fcb09e5c3984fc30
Jesus Zazueta <jjzazuet@gmail.com>
2025-03-09 03:21:57 -0400

    - Native build fix. - Patch version bump.

--- build.gradle.kts
+++ build.gradle.kts
@@ -3,7 +3,7 @@ plugins { id("io.vacco.oss.gitflow") version "1.0.1" apply(false) }
 subprojects {
   apply(plugin = "io.vacco.oss.gitflow")
   group = "io.vacco.awe4lb"
-  version = "0.5.0"
+  version = "0.5.1"
 
   configure<io.vacco.oss.gitflow.GsPluginProfileExtension> {
     addClasspathHell()
